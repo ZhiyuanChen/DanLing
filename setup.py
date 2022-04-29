8fbd7fd0f7a0aab64da9232d9132af0e751d55b0
Zhiyuan Chen <v-zhichen@microsoft.com>
2022-04-29 08:44:10 +0000

    first upload
    Signed-off-by: Zhiyuan Chen <v-zhichen@microsoft.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 
 setup(
     name='danling',
-    version='dev',
+    version='0.0',
     description='danling',
     author='Zhiyuan Chen',
     author_email='this@zyc.ai',
