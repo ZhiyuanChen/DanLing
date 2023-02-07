e04242d120030bad6700a863913e63e42b6a88d3
Zhiyuan Chen <this@zyc.ai>
2023-02-08 02:18:44 +0800

    allow scaling strategy of LRScheduler to be None
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- danling/optim/lr_scheduler/lr_scheduler.py
+++ danling/optim/lr_scheduler/lr_scheduler.py
@@ -11,12 +11,12 @@ class LRScheduler(lr_scheduler._LRScheduler):
         steps: int,
         final_lr: float = 1e-6,
         min_lr: float = 1e-6,
-        strategy: str = "cosine",
+        strategy: Optional[str] = "cosine",
         warmup_steps: int = 10_000,
         last_epoch: int = -1,
     ):
-        if strategy not in ("constant", "cosine", "linear"):
-            raise ValueError(f"Warmup strategy should be one of ('constant', 'linear', 'cosine'), but got {strategy}")
+        if strategy not in ("constant", "cosine", "linear", None):
+            raise ValueError(f"Scaling strategy should be one of ('constant', 'linear', 'cosine'), but got {strategy}")
         self.steps = steps
         self.final_lr = final_lr
         self.min_lr = min_lr
@@ -27,7 +27,7 @@ class LRScheduler(lr_scheduler._LRScheduler):
     def get_lr(self) -> List[float]:
         progress = (self._step_count - self.warmup_steps) / float(self.steps - self.warmup_steps)
         progress = np.clip(progress, 0.0, 1.0)
-        ratio = getattr(self, self.strategy)(progress)
+        ratio = getattr(self, self.strategy)(progress) if self.strategy is not None else 1.0
         if self.warmup_steps:
             ratio = ratio * np.minimum(1.0, self._step_count / self.warmup_steps)
         return [max(self.min_lr, lr * ratio) for lr in self.base_lrs]
