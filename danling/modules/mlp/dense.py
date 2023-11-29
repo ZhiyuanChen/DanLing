e7b1fdd83e5cbf934890dc53636f1f10e2be94f7
Zhiyuan Chen <this@zyc.ai>
2023-11-29 07:02:19 +0000

    make MLP and Dense more flexible
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- danling/modules/mlp/dense.py
+++ danling/modules/mlp/dense.py
@@ -6,24 +6,26 @@ class Dense(nn.Module):
         self,
         in_features: int,
         out_features: int,
-        dropout: float = 0.1,
         norm: str = "LayerNorm",
         activation: str = "ReLU",
+        dropout: float = 0.1,
+        pool: str = "AdaptiveAvgPool1d",
         bias: bool = True,
         residual: bool = True,
     ) -> None:
         super().__init__()
+        self.residual = residual
         self.linear = nn.Linear(in_features, out_features, bias=bias)
-        self.norm = getattr(nn, norm)(out_features)
-        self.activation = getattr(nn, activation)()
+        self.norm = getattr(nn, norm)(out_features) if norm else nn.Identity()
+        self.activation = getattr(nn, activation)() if activation else nn.Identity()
         self.dropout = nn.Dropout(dropout)
-        self.pool = nn.AdaptiveAvgPool1d(out_features) if residual else None
+        self.pool = getattr(nn, pool)(out_features) if pool else nn.Identity() if self.residual else None
 
     def forward(self, x):
         out = self.linear(x)
         out = self.norm(out)
         out = self.activation(out)
         out = self.dropout(out)
-        if self.pool is not None:
+        if self.residual:
             out = out + self.pool(x)
         return out
