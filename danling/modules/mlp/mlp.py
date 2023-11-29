848cde6bc3cd8cde83bf6c44c2bdc1fc98c2f463
Zhiyuan Chen <this@zyc.ai>
2023-11-29 07:02:19 +0000

    make MLP and Dense more flexible
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- danling/modules/mlp/mlp.py
+++ danling/modules/mlp/mlp.py
@@ -1,5 +1,5 @@
 from functools import partial
-from typing import List
+from typing import Sequence
 
 from torch import nn
 
@@ -9,28 +9,35 @@ from .dense import Dense
 class MLP(nn.Module):
     def __init__(  # pylint: disable=R0913
         self,
-        features: List[int],
-        dropout: float = 0.1,
+        *features: Sequence[int],
         norm: str = "LayerNorm",
         activation: str = "ReLU",
+        dropout: float = 0.1,
+        pool: str = "AdaptiveAvgPool1d",
         bias: bool = True,
         residual: bool = True,
+        linear_output: bool = True
     ) -> None:
         super().__init__()
+        if len(features) == 0 and isinstance(features, Sequence):
+            features = features[0]  # type: ignore[assignment]
         if not len(features) > 1:
             raise ValueError(f"`features` of MLP should have at least 2 elements, but got {len(features)}")
         dense = partial(
             Dense,
-            dropout=dropout,
             norm=norm,
             activation=activation,
+            dropout=dropout,
+            pool=pool,
             bias=bias,
             residual=residual,
         )
-        self.layers = nn.Sequential(
-            *[dense(in_features, out_features) for in_features, out_features in zip(features, features[1:])]
-        )
+        if linear_output:
+            layers = [dense(in_features, out_features) for in_features, out_features in zip(features, features[1:-1])]
+            layers.append(nn.Linear(features[-2], features[-1], bias=bias))
+        else:
+            layers = [dense(in_features, out_features) for in_features, out_features in zip(features, features[1:])]
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, x):  # pylint: disable=C0103
-        out = self.layers(x)
-        return out
+        return self.layers(x)
