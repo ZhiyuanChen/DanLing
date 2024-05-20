c5d22a608483cc67a371ce9ad86162f643197126
Zhiyuan Chen <this@zyc.ai>
2024-05-20 20:50:18 +0800

    add license header
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- danling/utils/decorators.py
+++ danling/utils/decorators.py
@@ -1,3 +1,20 @@
+# DanLing
+# Copyright (C) 2022-Present  DanLing
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the following licenses:
+# - The Unlicense
+# - GNU Affero General Public License v3.0 or later
+# - GNU General Public License v2.0 or later
+# - BSD 4-Clause "Original" or "Old" License
+# - MIT License
+# - Apache License 2.0
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the LICENSE file for more details.
+
 from __future__ import annotations
 
 import traceback
