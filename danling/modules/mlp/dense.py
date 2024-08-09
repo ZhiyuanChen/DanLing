654a392e2798429acab6276d8b6dc2ea80c8b939
Zhiyuan Chen <this@zyc.ai>
2024-08-09 16:24:01 +0800

    make AcceleratRunner a subclass of Accelerator add TorchRunner add DeepSpeedRunner

--- danling/modules/mlp/dense.py
+++ danling/modules/mlp/dense.py
@@ -33,10 +33,10 @@ class Dense(nn.Module):
         super().__init__()
         self.residual = residual
         self.linear = nn.Linear(in_features, out_features, bias=bias)
-        self.norm = getattr(nn, norm)(out_features) if norm else nn.Identity()
-        self.activation = getattr(nn, activation)() if activation else nn.Identity()
+        self.norm = getattr(nn, norm)(out_features) if norm else None
+        self.activation = getattr(nn, activation)() if activation else None
         self.dropout = nn.Dropout(dropout)
-        self.pool = getattr(nn, pool)(out_features) if pool else nn.Identity() if self.residual else None
+        self.pool = getattr(nn, pool)(out_features) if self.residual else None
 
     def forward(self, x):
         out = self.linear(x)
