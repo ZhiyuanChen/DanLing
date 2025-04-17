e6db0e5e7c3d5aab4e7f13d88812974e1ba3e6b6
Zhiyuan Chen <this@zyc.ai>
2025-04-17 21:31:04 +0800

    add to_device
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

---
+++ danling/data/__init__.py
@@ -0,0 +1,22 @@
+# DanLing
+# Copyright (C) 2022-Present  DanLing
+
+# This file is part of DanLing.
+
+# DanLing is free software: you can redistribute it and/or modify
+# it under the terms of the following licenses:
+# - The Unlicense
+# - GNU Affero General Public License v3.0 or later
+# - GNU General Public License v2.0 or later
+# - BSD 4-Clause "Original" or "Old" License
+# - MIT License
+# - Apache License 2.0
+
+# DanLing is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the LICENSE file for more details.
+
+from .utils import to_device
+
+__all__ = ["to_device"]
